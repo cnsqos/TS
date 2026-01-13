8720c62fdeac5938dd7c87d1add72b1d7cea4afd
cnsqo <cnsqo0815@gmail.com>
2026-01-13 21:48:13 +0900

    add
    add

--- billiard-scoreboard/src/app/page.tsx
+++ billiard-scoreboard/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <h1>🎱 테이블 선택 🎱</h1>
       <div className="table-buttons">
-        {Array.from({ length: 6 }, (_, i) => (
+        {Array.from({ length: 9 }, (_, i) => (
           <Link key={i} href={`/table/${i+1}`} className="table-btn">
             {i+1}번 테이블
           </Link>
